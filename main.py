78a12224adce01232e5dd231f5e7797f7a264a13
approachopen <approachopen@gmail.com>
2016-11-25 09:51:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! Roger Gao'
+    return 'Hello World! Roger Gao Test123'
 
 
 @app.errorhandler(404)
