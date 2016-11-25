0dac6c709ade0e74ba7abc644f606d3ed08199ba
Roger Gao <Roger Gao>
2016-11-25 09:37:22 +0800

    Be Used for Test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World! Roger Gao'
 
 
 @app.errorhandler(404)
